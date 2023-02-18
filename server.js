bdbef49de511e229550a3912a7d061ef37028d3d
fabricio.sousa <fabriciopsousa@gmail.com>
2023-02-17 22:49:27 -0300

    add mod nodemon

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const http = require("http")
 const port = 3000;
 
+const rotas = {
+    '/': 'Curso de Node',
+    '/livros': 'Bem vindo a pagina de livros',
+    '/autores': 'Listagem de autores',
+    '/editora': 'Pagina de editora',
+    '/sobre': 'Info sobre o projeto'
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Curso de Node');
+    res.end(rotas[req.url]);
 })
 
 server.listen(port, () =>{
